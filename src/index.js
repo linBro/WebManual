82c53647359ac293336d9e6ab046d29dfa551418
DOBEST\linjiyu <linjiyu@dobest.com>
2021-07-05 18:23:58 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import _ from 'lodash';
+import './style.css';
+import img from "./icon1.png";
+import Data from './data.xml';
+import Notes from './data.csv';
+
 function component() {
     const element = document.createElement('div');
   
     // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // 将图像添加到我们已经存在的 div 中。
+    const myIcon = new Image();
+    myIcon.src = img;
+    element.appendChild(myIcon);
+    
+    console.log(Data);
+    console.log(Notes);
   
     return element;
   }
   
   document.body.appendChild(component());
+
+
+  //https://webpack.docschina.org/guides/asset-management/
